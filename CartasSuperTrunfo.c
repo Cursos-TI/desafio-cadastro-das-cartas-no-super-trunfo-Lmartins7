9b23c240132795cc0bda9889784f69b85dc77b35
Lmartins7 <luismartins7797@gmail.com>
2025-04-21 13:47:26 +0000

    desafio_aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,20 +4,21 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     int populacao1, pontos_turisticos1, populacao2, pontos_turisticos2;
-    float area1, pib1, area2, pib2;
+    float area1, pib1, area2, pib2, denspop1, pibpc1, denspop2, pibpc2;
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     printf("Olá, vamos iniciar o desafio das cartas do Super trufo!\n");
     printf("Vamos iniciar cadastrando as cartas.\n");
-    printf("Para a carta 01:\n");
+    printf("---------------------------------------\n");
+
+    printf("** Carta 01 **\n");
     printf("Digite a População: ");
     scanf("%d", &populacao1);
     printf("Digite a Área: ");
@@ -26,8 +27,9 @@ int main() {
     scanf("%f", &pib1);
     printf("Digite o numero de pontos turísticos: ");
     scanf("%d", &pontos_turisticos1);
+    printf("---------------------------------------\n");
 
-    printf("Para a carta 02:\n");
+    printf("** Carta 02 **\n");
     printf("Digite a População: ");
     scanf("%d", &populacao2);
     printf("Digite a Área: ");
@@ -36,14 +38,24 @@ int main() {
     scanf("%f", &pib2);
     printf("Digite o numero de pontos turísticos: ");
     scanf("%d", &pontos_turisticos2);
+    printf("---------------------------------------\n");
+
+    denspop1 = (float) populacao1 / area1;
+    denspop2 = (float) populacao2 / area2;
+    pibpc1 = (float) pib1 / populacao1;
+    pibpc2 = (float) pib2 / populacao2;
 
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-printf("Obrigado por cadastrar as cartas!\n");
-printf("A primeira carta cadastrada foi:\n");
-printf("Carta 01:\n População: %d\n Área: %f\n PIB: %f\n Pontos turísticos: %d\n", populacao1, area1, pib1, pontos_turisticos1);
-printf("Carta 02:\n População: %d\n Área: %f\n PIB: %f\n Pontos turísticos: %d\n", populacao2, area2, pib2, pontos_turisticos2);
+    printf("Obrigado por cadastrar as cartas!\n");
+    printf("As cartas cadastradas foram:\n");
+    printf("---------------------------------------\n");
+    printf("** Carta 01 **\n População: %d\n Área: %.2f\n PIB: %.2f\n Pontos turísticos: %d\n Densidade populacional: %.2f\n PIB per capta: %.2f\n", populacao1, area1, pib1, pontos_turisticos1, denspop1, pibpc1);
+    printf("---------------------------------------\n");
+    printf("** Carta 02 **\n População: %d\n Área: %.2f\n PIB: %.2f\n Pontos turísticos: %d\n Densidade populacional: %.2f\n PIB per capta: %.2f\n", populacao2, area2, pib2, pontos_turisticos2, denspop2, pibpc2);
+    printf("---------------------------------------\n");
+    
     return 0;
 }
